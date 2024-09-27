528ea487c8d8c12974786548ce117f6aa6465258
TuanDatCoder <tuandatdq03@gmail.com>
2024-09-27 10:20:57 +0700

    Update Entity

--- src/main/java/com/example/marinepath/entity/Enum/DeliveryStatusEnum.java
+++ src/main/java/com/example/marinepath/entity/Enum/DeliveryStatusEnum.java
@@ -3,5 +3,6 @@ package com.example.marinepath.entity.Enum;
 public enum DeliveryStatusEnum {
     PENDING,      // Chưa giao nhận
     DELIVERED,    // Đã giao nhận thành công
-    CANCELED      // Giao nhận bị hủy
+    CANCELED,      // Giao nhận bị hủy
+    DELETED
 }
