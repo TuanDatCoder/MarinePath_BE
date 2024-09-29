3ebf0fee0b83c5dd267865c5866902b802a6b3b7
hantn127 <hanntn072003@gmail.com>
2024-09-29 15:41:50 +0700

    CRUD Ship

--- src/main/java/com/example/marinepath/repository/ShipRepository.java
+++ src/main/java/com/example/marinepath/repository/ShipRepository.java
@@ -1,10 +1,12 @@
 package com.example.marinepath.repository;
 
+import com.example.marinepath.entity.Enum.Ship.ShipStatusEnum;
 import com.example.marinepath.entity.Ship;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ShipRepository extends JpaRepository<Ship, Integer> {
 
-
-
+    List<Ship> findByStatusNot(ShipStatusEnum shipStatusEnum);
 }
