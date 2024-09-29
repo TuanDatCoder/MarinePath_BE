845b4c53ec7135395d5ee854db329e118a8f461a
TuanDatCoder <tuandatdq03@gmail.com>
2024-09-29 17:45:43 +0700

    feat CRUD Container Receipt

---
+++ src/main/java/com/example/marinepath/repository/ContainerRepository.java
@@ -0,0 +1,8 @@
+package com.example.marinepath.repository;
+
+import com.example.marinepath.entity.Container;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContainerRepository extends JpaRepository<Container, Integer> {
+
+}
