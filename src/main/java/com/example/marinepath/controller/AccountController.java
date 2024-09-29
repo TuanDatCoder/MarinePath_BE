07a85749561860150fcadb7455625609349e8f9d
TuanDatCoder <tuandatdq03@gmail.com>
2024-09-29 18:42:25 +0700

    feat/CRUD Account

--- src/main/java/com/example/marinepath/controller/AccountController.java
+++ src/main/java/com/example/marinepath/controller/AccountController.java
@@ -1,14 +1,13 @@
 package com.example.marinepath.controller;
 
 import com.example.marinepath.dto.Account.AccountResponseDTO;
+import com.example.marinepath.dto.Account.AccountUpdateResponseDTO;
 import com.example.marinepath.dto.ApiResponse;
 import com.example.marinepath.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -22,4 +21,28 @@ public class AccountController {
         ApiResponse<AccountResponseDTO> response = accountService.getCurrentAccount();
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<AccountResponseDTO>> updateAccount(@PathVariable Integer id, @RequestBody AccountUpdateResponseDTO accountUpdateResponseDTO) {
+        ApiResponse<AccountResponseDTO> response = accountService.updateAccount(id, accountUpdateResponseDTO);
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>> deleteAccount(@PathVariable Integer id) {
+        ApiResponse<Void> response = accountService.deleteAccount(id);
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<AccountResponseDTO>> getAccountById(@PathVariable Integer id) {
+        ApiResponse<AccountResponseDTO> response = accountService.getAccountById(id);
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<ApiResponse<AccountResponseDTO>> getAccountByEmail(@PathVariable String email) {
+        ApiResponse<AccountResponseDTO> response = accountService.getAccountByEmail2(email);
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+
 }
