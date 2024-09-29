0e22a290fcfe53c671f44cb61085026dfcb8be32
hantn127 <hanntn072003@gmail.com>
2024-09-29 15:42:49 +0700

    CRUD Ship

---
+++ src/main/java/com/example/marinepath/controller/ShipController.java
@@ -0,0 +1,49 @@
+package com.example.marinepath.controller;
+
+import com.example.marinepath.dto.ApiResponse;
+import com.example.marinepath.dto.Ship.ShipRequestDTO;
+import com.example.marinepath.dto.Ship.ShipResponseDTO;
+import com.example.marinepath.service.ShipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/ships")
+public class ShipController {
+    @Autowired
+    private ShipService shipService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<ShipResponseDTO>> createShip(@RequestBody ShipRequestDTO shipRequestDTO) {
+        ApiResponse<ShipResponseDTO> response = shipService.createShip(shipRequestDTO);
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<ShipResponseDTO>> updateShip(@PathVariable Integer id, @RequestBody ShipRequestDTO shipRequestDTO) {
+        ApiResponse<ShipResponseDTO> response = shipService.updateShip(id, shipRequestDTO);
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>> deleteShip(@PathVariable Integer id) {
+        ApiResponse<Void> response = shipService.deleteShip(id);
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<ShipResponseDTO>> getShipById(@PathVariable Integer id) {
+        ApiResponse<ShipResponseDTO> response = shipService.getShipById(id);
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<ShipResponseDTO>>> getAllTrips() {
+        ApiResponse<List<ShipResponseDTO>> response = shipService.getAllShips();
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+}
---
+++ src/main/java/com/example/marinepath/dto/Ship/ShipResponseDTO.java
@@ -0,0 +1,26 @@
+package com.example.marinepath.dto.Ship;
+
+import com.example.marinepath.entity.Enum.Ship.ShipStatusEnum;
+import com.example.marinepath.entity.Enum.Ship.ShipTypeEnum;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ShipResponseDTO {
+    private Integer id;
+
+    @JsonProperty("companyId")
+    private Integer companyId;
+
+    private String shipCode;
+    private String name;
+    private Integer capacity;
+    private Integer buildYear;
+    private String flag;
+    private ShipTypeEnum type;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private ShipStatusEnum status;
+}
